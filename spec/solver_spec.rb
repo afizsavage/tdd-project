263dcc28448fe760a647f42aefca4bac876e9a46
Afiz Savage <savageafiz9@gmail.com>
2022-03-25 15:53:55 +0000

    Refactored test for Solver class to use the expect syntax

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../solver'
 
-describe Solver do
+describe 'New Solver Instace' do
   before :each do
     @solver = Solver.new
   end
 
   it 'should be an instance of the solver class' do
-    @solver.should be_an_instance_of Solver
+    expect(@solver).to be_an_instance_of Solver
   end
 end
